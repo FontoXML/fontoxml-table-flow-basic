4c8fc2b5a02efb8c278da710c308cf5538a526b8
sergio.vela <sergio.vela@liones.nl>
2021-06-11 07:59:19 +0000

    DEV-4448 Make the allowExpansionInContentView option more findable

--- src/configureAsBasicTableElements.js
+++ src/configureAsBasicTableElements.js
@@ -105,39 +105,57 @@ import BasicTableDefinition from './table-definition/BasicTableDefinition.js';
  * @fontosdk
  * @category  add-on/fontoxml-table-flow-basic
  *
- * @param  {SxModule}        sxModule
- * @param  {Object}          options
- * @param  {number}          [options.priority]                             Selector priority for all elements configured by this function
- * @param  {Object}          options.table                                  Configuration for the table element
- * @param  {string}          options.table.localName                        The name of the table element
- * @param  {string}          [options.table.namespaceURI=null]              The namespace URI of the table element
- * @param  {XPathTest}       [options.table.tableFilterSelector]            Additional override for which basic table elements should be regarded as tables. This can be used to configure conreffed tables as not being tables.
- * @param  {Object}          [options.headerRow]                            Configuration for the header row element
- * @param  {string}          [options.headerRow.localName]                  The name of the header row element
- * @param  {string}          [options.headerRow.namespaceURI=null]          The namespace URI of the header row element
- * @param  {Object}          options.row                                    Configuration for the whole row
- * @param  {string}          options.row.localName                          The name of the row element
- * @param  {string}          [options.row.namespaceURI=null]                The namespace URI of the row element
- * @param  {Object}          [options.headerCell]                           Configuration for the header cell element. This option is optional. If it is not set, the cell option will be used for headerCell.
- * @param  {string}          [options.headerCell.localName]                 The name of the header cell element
- * @param  {string}          [options.headerCell.namespaceURI=null]         The namespace URI of the header cell
- * @param  {string}          [options.headerCell.defaultTextContainer=null] The default text container for the header cell element
- * @param  {Object}          options.cell                                   Configuration for the cell element
- * @param  {string}          options.cell.localName                         The name of the cell element
- * @param  {string}          [options.cell.namespaceURI=null]               The namespace URI of the cell element
- * @param  {string}          [options.cell.defaultTextContainer=null]       The default text container for the cell element
- * @param  {boolean}         [options.showInsertionWidget=false]            To add insertion buttons which insert a column or a row to a specific place, default false.
- * @param  {boolean}         [options.showHighlightingWidget=false]   	    To add highlighting bars which highlight columns and rows, and provide operations popover, default false.
- * @param  {WidgetSubAreaByName|Widget[]|null}  [options.rowBefore]         Used to add a single icon widget
- * before each row using {@link createIconWidget}. Row widgets are linked to the row
- * elements of the table. Any widget can be added but only icon widget is supported.
- * @param  {WidgetSubAreaByName|Widget[]|null}  [options.columnBefore]      Used to add one or multiple
- * widgets before each column. The context node of each column widget is the cell element in the first row. {@link fonto-documentation/docs/editor/api/index.xml#id-9d2b1ad5-bbc1-6c44-d491-16dc213c53f2 All widgets} are supported.
- * @param  {Object[]|null}   [options.columnWidgetMenuOperations]           To configure table widget menu for columns. It accepts an array of {@link ContextualOperation}s, but only supports "name" and "contents" properties. It is allowed to have only one layer of menu.
- * @param  {Object[]|null}   [options.rowWidgetMenuOperations]              To configure table widget menu for rows. It accepts an array of {@link ContextualOperation}s, but only supports "name" and "contents" properties. It is allowed to have only one layer of menu.
- * @param  {boolean}         [options.useDefaultContextMenu=true]           Whether or not to use a preconfigured context menu for elements within the table
- * @param  {XPathQuery}      [options.isCollapsibleQuery=false()]           The {@link XPathQuery} to determine whether or not a table has the ability to be collapsible. Optional, defaults to 'false()'. $rowCount and $columnCount helper variables can optionally be used in this XPath expression which evaluate to the total rows and total columns in a table.
- * @param  {XPathQuery}      [options.isInitiallyCollapsedQuery=true()]     The {@link XPathQuery} to determine whether or not a table should initially start off as collapsed. Tables must first have the ability to be collapsible with isCollapsibleQuery. Optional, defaults to 'true()'. $rowCount and $columnCount helper variables can optionally be used in this XPath expression which evaluate to the total rows and total columns in a table.
+ * @param  {SxModule}                           sxModule
+ * @param  {Object}                             options
+ * @param  {number}                             [options.priority]                              Selector priority for all elements configured by this function.
+ * @param  {AllowExpansionInContentView}        [options.allowExpansionInContentView]           Defines the availability of expansion of a table.
+ * @param  {Object}                             options.table                                   Configuration for the table element.
+ * @param  {string}                             options.table.localName                         The name of the table element.
+ * @param  {string}                             [options.table.namespaceURI=null]               The namespace URI of the table element.
+ * @param  {XPathTest}                          [options.table.tableFilterSelector]             Additional override for which basic table elements should be regarded.
+ *                                                                                              as tables. This can be used to configure conreffed tables as not being tables.
+ * @param  {Object}                             [options.headerRow]                             Configuration for the header row element.
+ * @param  {string}                             [options.headerRow.localName]                   The name of the header row element.
+ * @param  {string}                             [options.headerRow.namespaceURI=null]           The namespace URI of the header row element.
+ * @param  {Object}                             options.row                                     Configuration for the whole row.
+ * @param  {string}                             options.row.localName                           The name of the row element.
+ * @param  {string}                             [options.row.namespaceURI=null]                 The namespace URI of the row element.
+ * @param  {Object}                             [options.headerCell]                            Configuration for the header cell element. This option is optional.
+ *                                                                                              If it is not set, the cell option will be used for headerCell.
+ * @param  {string}                             [options.headerCell.localName]                  The name of the header cell element.
+ * @param  {string}                             [options.headerCell.namespaceURI=null]          The namespace URI of the header cell.
+ * @param  {string}                             [options.headerCell.defaultTextContainer=null]  The default text container for the header cell element.
+ * @param  {Object}                             options.cell                                    Configuration for the cell element.
+ * @param  {string}                             options.cell.localName                          The name of the cell element.
+ * @param  {string}                             [options.cell.namespaceURI=null]                The namespace URI of the cell element.
+ * @param  {string}                             [options.cell.defaultTextContainer=null]        The default text container for the cell element.
+ * @param  {boolean}                            [options.showInsertionWidget=false]             To add insertion buttons which insert a column or a row to
+ *                                                                                              a specific place, default false.
+ * @param  {boolean}                            [options.showHighlightingWidget=false]          To add highlighting bars which highlight columns and rows, and
+ *                                                                                              provide operations popover, default false.
+ * @param  {WidgetSubAreaByName|Widget[]|null}  [options.rowBefore]                             Used to add a single icon widget before each row using
+ *                                                                                              {@link createIconWidget}. Row widgets are linked to the row elements
+ *                                                                                              of the table. Any widget can be added but only icon widget is supported.
+ * @param  {WidgetSubAreaByName|Widget[]|null}  [options.columnBefore]                          Used to add one or multiple widgets before each column. The context
+ *                                                                                              node of each column widget is the cell element in the first row.
+ *                                                                                              {@link fonto-documentation/docs/editor/api/index.xml#id-9d2b1ad5-bbc1-6c44-d491-16dc213c53f2 All widgets}
+ *                                                                                              are supported.
+ * @param  {Object[]|null}                      [options.columnWidgetMenuOperations]            To configure table widget menu for columns. It accepts an array of
+ *                                                                                              {@link ContextualOperation}s, but only supports "name" and "contents"
+ *                                                                                              properties. It is allowed to have only one layer of menu.
+ * @param  {Object[]|null}                      [options.rowWidgetMenuOperations]               To configure table widget menu for rows. It accepts an array of
+ *                                                                                              {@link ContextualOperation}s, but only supports "name" and "contents" properties.
+ *                                                                                              It is allowed to have only one layer of menu.
+ * @param  {boolean}                            [options.useDefaultContextMenu=true]            Whether or not to use a preconfigured context menu for elements within the table.
+ * @param  {XPathQuery}                         [options.isCollapsibleQuery=false()]            The {@link XPathQuery} to determine whether or not a table has the ability
+ *                                                                                              to be collapsible. Optional, defaults to 'false()'. $rowCount and $columnCount
+ *                                                                                              helper variables can optionally be used in this XPath expression which evaluate
+ *                                                                                              to the total rows and total columns in a table.
+ * @param  {XPathQuery}                         [options.isInitiallyCollapsedQuery=true()]      The {@link XPathQuery} to determine whether or not a table should initially
+ *                                                                                              start off as collapsed. Tables must first have the ability to be collapsible
+ *                                                                                              with isCollapsibleQuery. Optional, defaults to 'true()'. $rowCount and $columnCount
+ *                                                                                              helper variables can optionally be used in this XPath expression which evaluate
+ *                                                                                              to the total rows and total columns in a table.
  */
 export default function configureAsBasicTableElements(sxModule, options) {
 	options = options || {};
