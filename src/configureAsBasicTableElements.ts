cf1287e66db4c7388075c0bd08a404075e3b17b3
wvbe <wybe@x-54.com>
2021-12-15 10:47:29 +0000

    DEV-13332 Organize the types for table CVK better, transfer documented remarks

--- src/configureAsBasicTableElements.ts
+++ src/configureAsBasicTableElements.ts
@@ -1,11 +1,9 @@
 import type { SxModule } from 'fontoxml-modular-schema-experience/src/sxManager';
 import configureAsTableElements from 'fontoxml-table-flow/src/configureAsTableElements';
-import type {
-	TableElementsBasicOptions,
-	TableElementsSharedOptions,
-} from 'fontoxml-table-flow/src/types.configureAsTableElements';
+import type { TableElementsSharedOptions } from 'fontoxml-table-flow/src/types';
 
 import BasicTableDefinition from './table-definition/BasicTableDefinition';
+import type { TableElementsBasicOptions } from './types';
 
 /**
  * @remarks
@@ -115,79 +113,6 @@ import BasicTableDefinition from './table-definition/BasicTableDefinition';
  * | our guide} to learn more.
  *
  * @fontosdk importable
- *
- * @param sxModule                                -
- * @param options                                 -
- * @param options.priority                        - Selector priority for all elements configured by
- *                                                  this function.
- * @param options.allowExpansionInContentView     - Defines the availability of expansion of a table.
- * @param options.table                           - Configuration for the table element.
- * @param options.table.localName                 - The name of the table element.
- * @param options.table.namespaceURI              - The namespace URI of the table element.
- * @param options.table.tableFilterSelector       - Additional override for which basic table elements
- *                                                  should be regarded. as tables. This can be used to
- *                                                  configure conreffed tables as not being tables.
- * @param options.headerRow                       - Configuration for the header row element.
- * @param options.headerRow.localName             - The name of the header row element.
- * @param options.headerRow.namespaceURI          - The namespace URI of the header row element.
- * @param options.row                             - Configuration for the whole row.
- * @param options.row.localName                   - The name of the row element.
- * @param options.row.namespaceURI                - The namespace URI of the row element.
- * @param options.headerCell                      - Configuration for the header cell element. This
- *                                                  option is optional. If it is not set, the cell
- *                                                  option will be used for headerCell.
- * @param options.headerCell.localName            - The name of the header cell element.
- * @param options.headerCell.namespaceURI         - The namespace URI of the header cell.
- * @param options.headerCell.defaultTextContainer - The default text container for the header cell
- *                                                  element.
- * @param options.cell                            - Configuration for the cell element.
- * @param options.cell.localName                  - The name of the cell element.
- * @param options.cell.namespaceURI               - The namespace URI of the cell element.
- * @param options.cell.defaultTextContainer       - The default text container for the cell element.
- * @param options.showInsertionWidget             - To add insertion buttons which insert a column or a
- *                                                  row to a specific place, default false.
- * @param options.showHighlightingWidget          - This is deprecated. Instead use
- *                                                  showSelectionWidget.
- * @param options.showSelectionWidget             - To add selection bars which select columns and
- *                                                  rows, and provide operations popover, default
- *                                                  false.
- * @param options.rowBefore                       - Used to add a single icon widget before each row
- *                                                  using {@link createIconWidget}. Row widgets are
- *                                                  linked to the row elements of the table. Any widget
- *                                                  can be added but only icon widget is supported.
- * @param options.columnBefore                    - Used to add one or multiple widgets before each
- *                                                  column. The context node of each column widget is
- *                                                  the cell element in the first row. {@link
- *                                                  fonto-documentation/docs/generated-content/editor-api/index.xml#id-cd5577eb-9790-92d6-e3ac-8d1554fe6b12
- *                                                  | All widgets} are supported.
- * @param options.columnWidgetMenuOperations      - This is deprecated. Use columnsWidgetMenuOperations
- *                                                  instead.
- * @param options.columnsWidgetMenuOperations     - To configure table widget menu for columns. It accepts
- *                                                  an array of {@link ContextualOperation}s, but only
- *                                                  supports "name" and "contents" properties. It is
- *                                                  allowed to have only one layer of menu.
- * @param options.rowWidgetMenuOperations         - This is deprecated. Use rowsWidgetMenuOperations
- *                                                  instead.
- * @param options.rowsWidgetMenuOperations        - To configure table widget menu for rows. It accepts
- *                                                  an array of {@link ContextualOperation}s, but only
- *                                                  supports "name" and "contents" properties. It is
- *                                                  allowed to have only one layer of menu.
- * @param options.useDefaultContextMenu           - Whether or not to use a preconfigured context menu
- *                                                  for elements within the table.
- * @param options.isCollapsibleQuery              - The {@link XPathQuery} to determine whether or not
- *                                                  a table has the ability to be collapsible.
- *                                                  Optional, defaults to 'false()'. $rowCount and
- *                                                  $columnCount helper variables can optionally be
- *                                                  used in this XPath expression which evaluate to the
- *                                                  total rows and total columns in a table.
- * @param options.isInitiallyCollapsedQuery       - The {@link XPathQuery} to determine whether or not
- *                                                  a table should initially start off as collapsed.
- *                                                  Tables must first have the ability to be
- *                                                  collapsible with isCollapsibleQuery. Optional,
- *                                                  defaults to 'true()'. $rowCount and $columnCount
- *                                                  helper variables can optionally be used in this
- *                                                  XPath expression which evaluate to the total rows
- *                                                  and total columns in a table.
  */
 export default function configureAsBasicTableElements(
 	sxModule: SxModule,
---
+++ src/types.ts
@@ -0,0 +1,147 @@
+import type { DefaultTextContainer } from 'fontoxml-families/src/types';
+import type { XPathTest } from 'fontoxml-selectors/src/types';
+
+/**
+ * @remarks
+ * The options accepted by {@link configureAsBasicTableElements}. Please see
+ * {@link TableElementsSharedOptions} for more information on the other
+ * options accepted by this function.
+ *
+ * @fontosdk
+ */
+export type TableElementsBasicOptions = {
+	/**
+	 * @remarks
+	 * Configuration for the table element.
+	 */
+	table: {
+		/**
+		 * @remarks
+		 * The name of the table element.
+		 *
+		 * @fontosdk
+		 */
+		localName: string;
+		/**
+		 * @remarks
+		 * The namespace URI of the table element.
+		 *
+		 * @fontosdk
+		 */
+		namespaceURI?: string | null;
+		/**
+		 * @remarks
+		 * Additional override for which basic table elements
+		 * should be regarded. as tables. This can be used to
+		 * configure conreffed tables as not being tables.
+		 *
+		 * @fontosdk
+		 */
+		tableFilterSelector?: XPathTest;
+	};
+	/**
+	 * @remarks
+	 * Configuration for the header row element.
+	 *
+	 * @fontosdk
+	 */
+	headerRow?: {
+		/**
+		 * @remarks
+		 * The name of the header row element.
+		 *
+		 * @fontosdk
+		 */
+		localName?: string;
+		/**
+		 * @remarks
+		 * The namespace URI of the header row element.
+		 *
+		 * @fontosdk
+		 */
+		namespaceURI?: string | null;
+	};
+	/**
+	 * @remarks
+	 *  Configuration for the whole row.
+	 *
+	 * @fontosdk
+	 */
+	row: {
+		/**
+		 * @remarks
+		 * The name of the row element.
+		 *
+		 * @fontosdk
+		 */
+		localName: string;
+		/**
+		 * @remarks
+		 * The namespace URI of the row element.
+		 *
+		 * @fontosdk
+		 */
+		namespaceURI?: string | null;
+	};
+	/**
+	 * @remarks
+	 * Configuration for the header cell element. This
+	 * option is optional. If it is not set, the cell
+	 * option will be used for headerCell.
+	 *
+	 * @fontosdk
+	 */
+	headerCell?: {
+		/**
+		 * @remarks
+		 * The name of the header cell element.
+		 *
+		 * @fontosdk
+		 */
+		localName?: string;
+		/**
+		 * @remarks
+		 * The namespace URI of the header cell.
+		 *
+		 * @fontosdk
+		 */
+		namespaceURI?: string | null;
+		/**
+		 * @remarks
+		 *The default text container for the header cell
+		 *                                                  element.
+		 *
+		 * @fontosdk
+		 */
+		defaultTextContainer?: DefaultTextContainer;
+	};
+	/**
+	 * @remarks
+	 * Configuration for the cell element.
+	 *
+	 * @fontosdk
+	 */
+	cell: {
+		/**
+		 * @remarks
+		 * The name of the cell element.
+		 *
+		 * @fontosdk
+		 */
+		localName: string;
+		/**
+		 * @remarks
+		 * The namespace URI of the cell element.
+		 *
+		 * @fontosdk
+		 */
+		namespaceURI?: string | null;
+		/**
+		 * @remarks
+		 * The default text container for the cell element.
+		 *
+		 * @fontosdk
+		 */
+		defaultTextContainer?: DefaultTextContainer;
+	};
+};
